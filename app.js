788cc457f103ca004826c0a506f440f0cbeb35f3
HunterPsycho100 <67747295+HunterPsycho100@users.noreply.github.com>
2021-06-10 10:43:14 -0700

    updated app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var db = require('morgan');
+
 var app = express();
 
 // view engine setup
