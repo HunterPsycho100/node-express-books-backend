12e5b60d5ebf39d26fa5de5c66714d92d0ee5bd9
HunterPsycho100 <67747295+HunterPsycho100@users.noreply.github.com>
2021-06-12 21:11:30 -0700

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //for environ vars
 //require('dotenv').config();
-//var cors = require('cors');
+var cors = require('cors');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -18,7 +18,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-//app.use(cors());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
